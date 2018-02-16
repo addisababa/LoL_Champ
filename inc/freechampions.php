1257bf6d50e99623e7b0baa77316399ec8370b30
addisababa <mladen.mandic1989@gmail.com>
2018-02-16 03:30:04 +0100

    page for champions

---
+++ inc/freechampions.php
@@ -0,0 +1,17 @@
+<div class="freechampions">
+	<div class="freechampionshead">
+		<h2>Free Champions this week</h2>
+	</div>	
+	<div class="freechampionsbody">
+		<img src="img/ekko.jpg">
+		<img src="img/hecarim.jpg">
+		<img src="img/irelia.jpg">
+		<img src="img/jayce.jpg">
+		<img src="img/nami.jpg">
+		<img src="img/rammus.jpg">
+		<img src="img/sona.jpg">
+		<img src="img/ezreal.jpg">
+		<img src="img/corki.jpg">
+	
+	</div>
+</div>

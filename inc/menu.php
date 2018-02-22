7026ad33a211f2b16311e1a8c968ea040475e89e
addisababa <mladen.mandic1989@gmail.com>
2018-02-22 01:28:23 +0100

    changing html for falling menu

--- inc/menu.php
+++ inc/menu.php
@@ -3,11 +3,15 @@ League of Legends is a fast-paced, competitive online game that blends the speed
 
 <div id="contactdiv" class="about">
 	<form>
+	<div class="con">
 		<input type="text" placeholder="name">
 		<input type="text" placeholder="Mobile Number">
 		<input type="text" placeholder="Email">
-		<textarea>You Message </textarea>
-		<input type="submit" value="submit">
+		<a class="sb" href="#">Submit</a>
+		</div>
+		<div class="con">
+			<textarea>You Message </textarea>
+		</div>
 	</form>
 </div>
 <div class="fullwidth">
@@ -18,7 +22,18 @@ League of Legends is a fast-paced, competitive online game that blends the speed
 				<li class="<?= $ha;?>"><a href="#">Home</a></li>
 				<li class="<?= $na;?>"><a href="#">News</a></li>
 				<li class="<?= $ma;?>"><a href="#">Media</a></li>
-				<li class="<?= $ca;?>"><a href="#">Champions</a></li>
+				<li id="ch" class="<?= $ca;?>"><a href="#">Champions</a>
+					<ul id="chm">	
+						<li><a href="#">Assassin</a></li>
+						<li><a href="#">Fighter</a></li>
+						<li><a href="#">Mage</a></li>
+						<li><a href="#">Support</a></li>
+						<li><a href="#">Tank</a></li>
+						<li><a href="#">Marksman</a></li>
+				
+				
+					</ul>
+				</li>
 				<li class="<?= $ga;?>"><a href="#">Guides</a></li>
 				<li id="contact" class="<?= $coa;?>"><a href="#">Contact</a></li>
 				<li id="about" class="<?= $aa;?>"><a href="#">About</a></li>

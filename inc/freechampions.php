967c881dd4254f42e86eda2299c7f39d068c725a
addisababa <mladen.mandic1989@gmail.com>
2018-02-22 01:29:28 +0100

    new free champ

--- inc/freechampions.php
+++ inc/freechampions.php
@@ -1,17 +1,22 @@
 <div class="freechampions">
 	<div class="freechampionshead">
 		<h2>Free Champions this week</h2>
+		<span id="close">X</span>
 	</div>	
 	<div class="freechampionsbody">
-		<img src="img/ekko.jpg">
-		<img src="img/hecarim.jpg">
-		<img src="img/irelia.jpg">
-		<img src="img/jayce.jpg">
-		<img src="img/nami.jpg">
-		<img src="img/rammus.jpg">
-		<img src="img/sona.jpg">
-		<img src="img/ezreal.jpg">
-		<img src="img/corki.jpg">
+		<img onclick="championinfo('Ekko')" src="img/ekko.jpg">
+		<img onclick="championinfo('Hecarim')" src="img/hecarim.jpg">
+		<img onclick="championinfo('Irelia')" src="img/irelia.jpg">
+		<img onclick="championinfo('Jayce')" src="img/jayce.jpg">
+		<img onclick="championinfo('Nami')" src="img/nami.jpg">
+		<img onclick="championinfo('Rammus')" src="img/rammus.jpg">
+		<img onclick="championinfo('Sona')" src="img/sona.jpg">
+		<img onclick="championinfo('Ezreal')" src="img/ezreal.jpg">
+		<img onclick="championinfo('Corki')" src="img/corki.jpg">
 	
+	</div>
+	<div id="infohere" class="championinfo">
+		
+		
 	</div>
 </div>
